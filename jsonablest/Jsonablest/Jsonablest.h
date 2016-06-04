0be15a98c141c4c4f1732d7f069c02330f84078e
Antonio Bello <jeden@elapsus.com>
2016-06-05 00:11:58 +0200

    Creation of project, and tests target

---
+++ jsonablest/Jsonablest/Jsonablest.h
@@ -0,0 +1,19 @@
+//
+//  Jsonablest.h
+//  Jsonablest
+//
+//  Created by Antonio Bello on 6/4/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jsonablest.
+FOUNDATION_EXPORT double JsonablestVersionNumber;
+
+//! Project version string for Jsonablest.
+FOUNDATION_EXPORT const unsigned char JsonablestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jsonablest/PublicHeader.h>
+
+
